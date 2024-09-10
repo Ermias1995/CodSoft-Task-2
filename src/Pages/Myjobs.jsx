37d1fb04c5ee5e565294f3e703c5ad6e8a8aeea2
Ermias1995 <ermias.firdawok-ug@aau.edu.et>
2024-09-09 18:23:52 -0700

    My Jobs table Finished

--- src/Pages/Myjobs.jsx
+++ src/Pages/Myjobs.jsx
@@ -8,8 +8,8 @@ function Myjobs() {
     const [searchText, setSearchText] = useState("");
 
     useEffect(()=>{
-        fetch(`http://localhost:3000/my-jobs/:ermiascode@gmail.com`).
-        then(res => res.json())
+        fetch(`http://localhost:3000/myJobs/ermiascode@gmail.com`)
+        .then(res => res.json())
         .then(data => {
           setJobs(data);
         });
@@ -20,6 +20,7 @@ function Myjobs() {
       console.log(filter);
       setJobs(filter);
     }
+
   return (
     <div className="max-w-screen-2xl container mx-auto xl:px-24 px-4">
       <div className="my-jobs-container">
@@ -34,7 +35,6 @@ function Myjobs() {
           <button className="bg-secondary text-white font-semibold px-8 py-2 rounded" onChange={handleSearch}>Search</button>
         </div>
       </div>
-
       <section className="py-1 bg-blueGray-50">
         <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4 mx-auto mt-24">
           <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded ">
@@ -45,11 +45,10 @@ function Myjobs() {
                 </div>
                 <div className="relative w-full px-4 max-w-full flex-grow flex-1 text-right">
 
-                  <Link to="/post-job"><button className="bg-indigo-500 text-white active:bg-indigo-600 
+                  <Link to="/post-job"><button className="bg-secondary text-white 
                             text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear
                             transition-all duration-150" type="button">Post a new job</button>
                   </Link>
-
                 </div>
               </div>
             </div>
@@ -70,90 +69,31 @@ function Myjobs() {
                   <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
                     Salary
                   </th>
-                  <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                    Delete
-                  </th>
                   </tr>
                 </thead>
 
+
                 <tbody>
-                  <tr>
+                {
+                  jobs.map((job, index)=>(
+                  <tr key={index}>
                     <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700 ">
-                      /argon/
+                      {index + 1}
                     </th>
                     <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 ">
-                      4,569
+                      {job.jobTitle}
                     </td>
                     <td className="border-t-0 px-6 align-center border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                      340
+                      {job.companyName}
                     </td>
                     <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
                       <i className="fas fa-arrow-up text-emerald-500 mr-4"></i>
-                      46,53%
+                      {job.maxPrice}
                     </td>
                   </tr>
-                  <tr>
-                    <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700">
-                      /argon/index.html
-                    </th>
-                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                      3,985
-                    </td>
-                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                      319
-                    </td>
-                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                      <i className="fas fa-arrow-down text-orange-500 mr-4"></i>
-                      46,53%
-                    </td>
-                  </tr>
-                  <tr>
-                    <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700">
-                      /argon/charts.html
-                    </th>
-                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                      3,513
-                    </td>
-                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                      294
-                    </td>
-                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                      <i className="fas fa-arrow-down text-orange-500 mr-4"></i>
-                      36,49%
-                    </td>
-                  </tr>
-                  <tr>
-                    <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700">
-                      /argon/tables.html
-                    </th>
-                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                      2,050
-                    </td>
-                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                      147
-                    </td>
-                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                      <i className="fas fa-arrow-up text-emerald-500 mr-4"></i>
-                      50,87%
-                    </td>
-                  </tr>
-                  <tr>
-                    <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700">
-                      /argon/profile.html
-                    </th>
-                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                      1,795
-                    </td>
-                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                      190
-                    </td>
-                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                      <i className="fas fa-arrow-down text-red-500 mr-4"></i>
-                      46,53%
-                    </td>
-                  </tr>
-                </tbody>
-
+                  ))
+                }
+        </tbody>
               </table>
             </div>
           </div>
