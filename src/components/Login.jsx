edddb9ffacf945371417e612d912f690632eafdf
Ermias1995 <ermias.firdawok-ug@aau.edu.et>
2024-09-09 21:25:02 -0700

    Not done yet

---
+++ src/components/Login.jsx
@@ -0,0 +1,8 @@
+function Login() {
+  return (
+    <div className="h-screen w-full flex items-center justify-center">
+        <button className="bg-secondary px-8 py-2 text-white">Login</button>
+    </div>
+  )
+}
+export default Login
